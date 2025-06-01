28ebd95333c68d2df13ac72d5dc0120b178a9fab
A G <fdwnct@gmail.com>
2025-06-01 18:51:54 -0400

    wip

---
+++ tmp/cache/PHPStan/92/27/9227d09a67f924e101406c96d7127a000cf7b4ae.php
@@ -0,0 +1,53 @@
+<?php declare(strict_types = 1);
+
+// odsl-/Users/ag/Documents/Projects/ci-test/app
+return \PHPStan\Cache\CacheItem::__set_state(array(
+   'variableKey' => 'v1',
+   'data' => 
+  array (
+    '/Users/ag/Documents/Projects/ci-test/app/Providers/AppServiceProvider.php' => 
+    array (
+      0 => '01bf9e5cf5bb666446625056b618445ae4749675',
+      1 => 
+      array (
+        0 => 'app\\providers\\appserviceprovider',
+      ),
+      2 => 
+      array (
+        0 => 'app\\providers\\register',
+        1 => 'app\\providers\\boot',
+      ),
+      3 => 
+      array (
+      ),
+    ),
+    '/Users/ag/Documents/Projects/ci-test/app/Models/User.php' => 
+    array (
+      0 => '0de556dcaed737c421ea5b9b029f7cc1445768cf',
+      1 => 
+      array (
+        0 => 'app\\models\\user',
+      ),
+      2 => 
+      array (
+        0 => 'app\\models\\casts',
+      ),
+      3 => 
+      array (
+      ),
+    ),
+    '/Users/ag/Documents/Projects/ci-test/app/Http/Controllers/Controller.php' => 
+    array (
+      0 => 'f4afbe85426ee0213ba2e5778ae893f8ab37cf9a',
+      1 => 
+      array (
+      ),
+      2 => 
+      array (
+      ),
+      3 => 
+      array (
+      ),
+    ),
+  ),
+));
